22c9ac8d560b7fd982a53c692f25cca4f9c1a304
cacf10 <cacf10@163.com>
2025-12-29 15:01:39 +0800

    Add files via upload

---
+++ 03BeetleBot/angle.h
@@ -0,0 +1,18 @@
+#ifndef __ANHLE_H__
+
+#define __ANGLE_H__
+
+int angle0 = 93;
+int angle1 = 132;
+int angle2 = 57;
+int angle3 = 90;
+int angle4 = 99;
+int angle5 = 45;
+int angle6 = 96;
+int angle7 = 45;
+int angle8 = 114;
+int angle9 = 90;
+int angle10 = 96;
+int angle11 = 153;
+int angle12 = 90;
+#endif
